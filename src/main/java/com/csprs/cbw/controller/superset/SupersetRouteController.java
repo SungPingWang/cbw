c2063cd50c7d4edf86678222ab5d7a1108d00f51
SungPingWang <t124650401@gmail.com>
2022-09-24 12:26:02 +0800

    20220924 統一上傳一些之前沒有上傳到的東西

--- src/main/java/com/csprs/cbw/controller/superset/SupersetRouteController.java
+++ src/main/java/com/csprs/cbw/controller/superset/SupersetRouteController.java
@@ -61,12 +61,34 @@ public class SupersetRouteController {
 	
 	
 	@GetMapping("/redirectIframe")
-	public String redirectIframe(Map<String, Object> map) {
+	public String redirectIframe(Map<String, Object> map, HttpServletResponse response) {
 		map.put("chart1", "http://172.22.110.27:8787/superset/explore/p/jkx2A1nw6PW/?standalone=1&height=400");
 		map.put("chart2", "http://172.22.110.27:8787/superset/explore/p/ogJDA6Y72nx/?standalone=1&height=400");
 		map.put("chart3", "http://172.22.110.27:8787/superset/explore/p/W2lLwvNwomN/?standalone=1&height=400");
 		map.put("chart4", "http://172.22.110.27:8787/superset/explore/p/PZ6zwp0w1k8/?standalone=1&height=400");
+		/*
+		ObjectMapper objectMapper = new ObjectMapper();
+		ObjectNode node = objectMapper.createObjectNode();
+		node.put("username", "admin");
+		node.put("password", "Jacksei@123");
+		node.put("provider", SUPERSET_PROVIDER);
+		node.put("refresh", true);
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
 		
+		HttpEntity<String> request = new HttpEntity<String>(node.toString(), headers);
+		String url = "http://fa.idv.tw/api/v1/security/login";
+		ResponseEntity<SupersetResponse> postForEntity = restTemplate.postForEntity(url, request, SupersetResponse.class);
+		String url2 = "http://fa.idv.tw/superset/welcome/";
+		//String url2 = SUPERSET_URL +  "/superset/explore/p/kvP5m1Zl92O/";
+		map.put("tk", postForEntity.getBody().getAccess_token());*/
+		map.put("chart5", "http://fa.idv.tw/superset/explore/?form_data_key=hptktdko2HSEbN82N0nvjrCvXXeRQCOc9_lXcUaocRlgjHHAAR-cN5vlvcO0mVcj&slice_id=214&standalone=1&height=400");
+		
+		/*
+		response.setHeader("Access-Control-Allow-Origin", "http://localhost"); //允许来之域名为http://localhost的请求        
+	    response.setHeader("Access-Control-Allow-Headers", "Origin,No-Cache, X-Requested-With, If-Modified-Since, Pragma, Last-Modified, Cache-Control, Expires, Content-Type, X-E4M-With, userId, token");
+	    response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE"); //请求允许的方法
+	    response.setHeader("Access-Control-Max-Age", "3600");*/
 		return "superset/iframe.html";
 	}
 	
